9606e94f178658c7815d50c18e8b88a212215032
16fortis <emanuelortiz21@gmail.com>
2024-08-29 11:18:32 -0500

    update restart file

--- .platform/hooks/postdeploy/1-cwagent-depend.sh
+++ .platform/hooks/postdeploy/1-cwagent-depend.sh
@@ -10,9 +10,3 @@ check_cw_agent() {
 
 # Initial run to fetch CloudWatch Agent configuration
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:${SSM_PARAMETER_FILE}
-
-# Check and restart CloudWatch Agent if necessary
-check_cw_agent
-if [ $? -ne 0 ]; then
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:${SSM_PARAMETER_FILE}
-fi
--- .platform/hooks/postdeploy/restart_cw.sh
+++ .platform/hooks/postdeploy/restart_cw.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Fetch CloudWatch Agent configuration
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:${SSM_PARAMETER_FILE}
-
-# Function to check CloudWatch Agent status
-check_cw_agent() {
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status | grep -q 'running'
-}
-
 # Check and restart CloudWatch Agent if necessary
 check_cw_agent
 if [ $? -ne 0 ]; then
